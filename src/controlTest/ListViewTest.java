4a2061bfd818d7be905424c47d353febfb4c15e0
blingminho <tkdjun67@naver.com>
2018-03-15 21:52:13 +0900

    ListView 연습

--- src/controlTest/ListViewTest.java
+++ src/controlTest/ListViewTest.java
@@ -7,11 +7,14 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 public class ListViewTest extends Application {
 
@@ -46,6 +49,27 @@ public class ListViewTest extends Application {
 				}
 		);
 		
+		// ListView의 실제 내용은 변경되지 않고 화면에 보이는 내용을 변경하는 방법
+		list.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
+			
+			@Override
+			public ListCell<String> call(ListView<String> param) {
+				return new ListCell<String>() {// 익명 구현체 생성
+					@Override
+					protected void updateItem(String item, boolean empty) {
+						super.updateItem(item, empty);
+						Rectangle rect = new Rectangle(100, 20);
+						if (item != null) {
+							rect.setFill(Color.web(item));
+							// 변경되는 값이 문자열이면 setText()메서드를
+							// 변경되는 값이 문자열 이외의 객체이면 setGrphic()메서드를 사용하여 변경한다
+							setGraphic(rect);// 값 변경
+							
+						}
+					}
+				};
+			}
+		});
 		
 		Scene scene = new Scene(vbox);
 		primaryStage.setTitle("ListView연습");
